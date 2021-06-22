c3a34850a1216758c85ef5a9125b1da77edea77d
dung <dung@gmail.com>
2021-06-22 22:53:05 +0700

    update

---
+++ Lab6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.UTF8;
+            Console.WriteLine("Tìm nghiệm phương trình bậc hai");
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            float a, b, c;
+            Console.WriteLine("Nhập a: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập a: ");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập a: ");
+            c = float.Parse(Console.ReadLine());
+            float delta = b * b - 4 * a * c;
+            if (delta>0)
+            {
+                Console.WriteLine("Phương trình có 2 nghiệm: ");
+                Console.WriteLine("x1={0}",((-b-Math.Sqrt(delta))/2*a));
+                Console.WriteLine("x2={0}",((-b+Math.Sqrt(delta))/2*a));
+            }else if (delta==0)
+            {
+                Console.WriteLine("Phương trình có nghiệm kép");
+            }
+            else if (delta<0)
+            {
+                Console.WriteLine("Phương trình vô nghiệm");
+            }
+        }
+    }
+}
